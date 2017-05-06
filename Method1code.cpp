7d9c05c1d130887d5ada245dddf269e51abc27a6
evangearne <evangearne@myvuw.ac.nz>
2017-05-06 19:02:31 +1200

    Update Method1code.cpp

--- Method1code.cpp
+++ Method1code.cpp
@@ -4,25 +4,13 @@
 
 int main (){
 	init();
+	int w = 0;
+	int sum = 0;
 	while (true){
 		//Run Camera method
 		get.picture;
-		for (int x = 0;x <= 320;x = x + 10){ //This loop should only be to find where the white line is. Your way could work but only if we were reading from one point and it probably wouldn't be very reliable. It would also be better to use a completely different variable for the for loop, "i" for instance and keep the "x" variable inside the loop. Also if you use "<=" it will loop one time more than stated, just use "<".
-			int cam = get.pixel(x,120,3);
-			int methodRed = get.pixel((xPixelSize/2),y,0); //methodRed only needs to be used once each time we move and so should be outside of the "cam" loop, preferably before.
-			if (cam >= 127.5){
-				//Run motor forward for a specified time
-			}
-			else if (methodRed >= 127.5){
-				//Run IR method
-			}else{
-				if (x <= 160){
-					//turn robot right for a specified time
-				}
-				else{
-					//turn robot left for a specified time
-				}
-			}
-		}
+		for (i = -160; i > 160,i++)
+		w = set.pixel((i + 160),120,3)
+		sum = sum + i * w;
 	}
 }
