fbebbb94336ea76f6037b7abc8c027eaa3ed16e6
evangearne <evangearne@myvuw.ac.nz>
2017-05-08 14:39:24 +1200

    Corrections

--- Method1code.cpp
+++ Method1code.cpp
@@ -1,30 +1,31 @@
-	#include <stdio.h>
-        #include <time.h>
-        #include "E101.h"
+#include <stdio.h>
+#include <time.h>
+#include "E101.h"
 
-        int main (){
+int main (){
             init();
             for(;;){
                 float kd = 5;
                 float kp = 0.5;
-                x = 0;
-                sum = 0;
+                int x = 0;
+                int sum = 0;
                 int error = 0;
                 int previous_error;
                 int proportional_signal;
                 int derivative_signal;
                 int final_signal;
-                get.picture;
-                for (i=0; i<320; i++){
-                    if (get.pixel(x,y,3)>150){
+                int points;
+                take_picture();
+                for (int i = 0; i<320; i++){
+                    if (get_pixel(x,10,3)>150){
                         sum = sum + x;
                         points++;
                     }
                     x++;
                 }
                 error = (sum/points)-160;
-                sleep(0,100000);
-                if (-2<error<2){
+                sleep1(0,100000);
+                if(error==0){
                     set_motor(1, (0.5)*255);
                     set_motor(2, (0.5)*255);
                 }
@@ -38,4 +39,4 @@
                 }
 
             }
-        }
+} 
