d51c99a908ea34f1bfcdb419a17a32a3493c7bdf
spacemariney <spacemariney@hotmail.com>
2017-05-08 13:52:47 +1200

    New code for finding white line and using PID

--- Method1code.cpp
+++ Method1code.cpp
@@ -4,13 +4,37 @@
 
 int main (){
 	init();
-	int w = 0;
-	int sum = 0;
-	while (true){
-		//Run Camera method
+	for(;;){
+		float kd = 5;
+		float kp = 0.5;
+		x = 0;
+		sum = 0;
+		int error = 0;
+		int previous_error;
+		int proportional_signal;
+		int derivative_signal;
+		int final_signal;
 		get.picture;
-		for (i = -160; i > 160,i++)
-		w = set.pixel((i + 160),120,3)
-		sum = sum + i * w;
-	}
+		for (i=0; i<320; i++){
+			if (get.pixel(x,y,3)>150){
+				sum = sum + x;
+				points++;
+				}
+			x++;
+			}
+		error = (sum/points)-160;
+		sleep(0,100000);
+		if (-2<error<2){
+			set_motor(1, (0.5)*255);
+			set_motor(2, (0.5)*255);
+		}
+		else{
+		proportional_signal = error*kp;
+		derivative_signal = (error-previous_error/0.1)*kd;
+		previous_error = error;
+		final_signal = (proportional_signal+derivative_signal)/2;
+		set_motor(1, (final_signal/(160*kp))*255);
+		set_motor(2, (final_signal/(160*kp))*255);
+		}
+		
 }
