0f1766d499c46a1461c3bbc4dff5497c851e3853
swansotara <taran.swanson98@gmail.com>
2017-05-15 13:51:52 +1200

    Update Method1code.cpp

--- Method1code.cpp
+++ Method1code.cpp
@@ -30,14 +30,14 @@ int main (){
             } else{
                         if(error==0){ //if line is straight ahead go straight
                         set_motor(1, (0.5)*255); //sets left motor to half speed forward
-                        set_motor(2, (-0.5)*255); //sets right motor     ''     ''
+                        set_motor(2, (0.5)*255); //sets right motor     ''     ''
                         }
             else{ //if line is not straight ahead turn toward it
                         proportional_signal = error*kp; //number between 0 and 80
                         derivative_signal = (error-previous_error/0.1)*kd; //number between 0 and 80
                         final_signal = proportional_signal+derivative_signal; //adds the signals together
                         set_motor(1, (final_signal/(160))*255); //sets left motor to a value between -255 and 255
-                        set_motor(2, (final_signal/(160))*255); //sets right motor    ''     ''
+                        set_motor(2, (-final_signal/(160))*255); //sets right motor    ''     ''
                         }
                         sleep1(0,100000); // waits for .1 secs
             }
