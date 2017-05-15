2267e4d9ac5dce55c534ee2048864662ea389489
swansotara <taran.swanson98@gmail.com>
2017-05-15 14:06:37 +1200

    Update Method1code.cpp

--- Method1code.cpp
+++ Method1code.cpp
@@ -18,29 +18,31 @@ int main (){
                         int points = 0;
                         char take_picture();
                         for (int i = 0; i<320; i++){ //loop to find position of white line
-                                    if (get_pixel(i,10,3)>150){ //if pixel is white
+                                    if (get_pixel(10,i,3)>150){ //if pixel is white
                                     sum = sum + i; //adds position of all white pixels
                                     points++; //counts white pixels
+                                    printf ("%d", i);
                                     }
                         }
             error = (sum/points)-160; //how far the white line is from camera centre
-            if(points = 0){
+            if(points==0){
                         set_motor(1, 126);
                         set_motor(2, -126);
             } else{
                         if(error==0){ //if line is straight ahead go straight
-                        set_motor(1, (0.5)*255); //sets left motor to half speed forward
-                        set_motor(2, (0.5)*255); //sets right motor     ''     ''
+                        set_motor(1, (0.5)*254); //sets left motor to half speed forward
+                        set_motor(2, (0.5)*254); //sets right motor     ''     ''
                         }
             else{ //if line is not straight ahead turn toward it
                         proportional_signal = error*kp; //number between 0 and 80
                         derivative_signal = (error-previous_error/0.1)*kd; //number between 0 and 80
                         final_signal = proportional_signal+derivative_signal; //adds the signals together
-                        set_motor(1, (final_signal/(160))*255); //sets left motor to a value between -255 and 255
-                        set_motor(2, (-final_signal/(160))*255); //sets right motor    ''     ''
+                        set_motor(1, (final_signal/(160))*254); //sets left motor to a value between -255 and 255
+                        set_motor(2, (-final_signal/(160))*254); //sets right motor    ''     ''
                         }
                         sleep1(0,100000); // waits for .1 secs
             }
             previous_error = error; //changes previous_error for next time around the loop
             }
+            return 0;
 } 
