35d5b802e12097943a441854f5fbafeec9223b12
Leuthowanj <wanja.leuthold@gmail.com>
2017-05-22 18:52:09 +1200

    uploaded the code as it is now and added all the other files to make it run. additionally added images of robot, photos of specific details and and screenshot of the terminal while the code is running.

--- Method1code.cpp
+++ Method1code.cpp
@@ -4,7 +4,7 @@
 
 //sets initial variables
 float kd = 0.2;
-float kp = 0.8;
+float kp = 0.7;
 float error = 0;
 float previous_error = 0;
 float proportional_signal = 0;
@@ -12,66 +12,67 @@ float derivative_signal = 0;
 float final_signal = 0;
 bool received = false;
 
-/**
-extern "C" int init(int d_lev);
-extern "C" int connect_to_server( char server_addr[15],int port);
-extern "C" int send_to_server(char message[24]);
-extern "C" int receive_from_server(char message[24]);
-*/
-
 int main (){
             init(); //initialises hardware
             printf("initialised ");
             
             char ip [15] = {'1','3','0','.','1','9','5','.','6','.','1','9','6',' ',' '};
-            printf("ip saved ");
+            printf("ip saved\n");
             char message [24] = {'P','l','e','a','s','e'};
-            printf("message saved ");
+            printf("message saved\n");
             connect_to_server(ip, 1024);
-            printf("connected ");
-            receive_from_server(message); //this may be buggy!
-   printf("received ");
-            send_to_server(message);
-            printf("sent ");
-printf("%s", message);
-sleep1(0,300000); //waits .3 sec
+            printf("connected\n");
+            //receive_from_server(message); //this may be buggy!
+            printf("received\n");
+            //send_to_server(message);
+            printf("sent\n");
+			printf("%s\n", message);
+			
             for(;;){ //infinite loop
                         int sum = 0;
                         int points = 0;
                         take_picture();
                         for (int i = 0; i<320; i++){ //loop to find position of white line
-                                    if (get_pixel(10,i,3)>50){ //if pixel is white
+                                    if (get_pixel(220,i,3)>120){ //if pixel is white
                                     sum = sum + i; //adds position of all white pixels
                                     points++; //counts white pixels
                                     }
                         }
             if(points==0){
-                        set_motor(1, 10);
-                        set_motor(2, -10);
+                        set_motor(1, -215);
+                        set_motor(2, -235);
+                        sleep1(0,400000); // waits for .1 secs
             }
             else{
                         error = (sum/points)-160; //how far the white line is from camera centre
                         printf("%f \n", error);
+                        
                         proportional_signal = error*kp; //number between 0 and 80
+                        /**
                         derivative_signal = (error-previous_error)*(kd/2); //number between 0 and 80
                         final_signal = proportional_signal+derivative_signal; //adds the signals together
+                        */
                         
                         if(error<=10 && error>=-10){ //if line is straight ahead go straight
-                                    set_motor(1, 30); //sets  motor to forward
-                                    set_motor(2, 30); //sets  motor ''     ''
+                                    set_motor(1, 215); //sets  motor to forward
+                                    set_motor(2, 215); //sets  motor ''     ''
                         }
                         else if (error > 10){ //if line is not straight ahead turn toward it 
-
-                                    //set_motor(1, (final_signal/(160))*30); //sets  motor to a value between -255 and 255
-                                    //set_motor(2, (final_signal/(160))*15); //sets  motor    ''     ''
-set_motor(1, 20 + final_signal);
-set_motor(2, 20);
-}
-else if (error < -10){
-                                set_motor(1,20);
-set_motor(2,20 - final_signal);  
-// set_motor(1, (final_signal/(160))*15);
+						/**
+                        set_motor(1, (final_signal/(160))*30); //sets  motor to a value between -255 and 255
+                        set_motor(2, (final_signal/(160))*15); //sets  motor    ''     ''
+                        */ 
+                        
+								set_motor(1, 215 - proportional_signal);
+								set_motor(2, 225);
+						}
+						else if (error < -10){
+                                set_motor(1,225);
+								set_motor(2,215 + proportional_signal);  
+								/**
+									// set_motor(1, (final_signal/(160))*15);
                                     // set_motor(2, (final_signal/(160))*30);
+                                    */ 
                         }
             }
        
